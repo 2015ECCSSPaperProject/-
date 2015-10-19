8bc513600111888090676f59632ee33f42784c94
Owataso <owatasosan001@gmail.com>
2015-10-19 16:28:20 +0900

    manager

---
+++ Paperona4ネット/Papera4ネット/Paper/client/source/Player/PlayerManager.h
@@ -0,0 +1,51 @@
+#pragma once
+
+class BasePlayer;
+
+class PlayerManager
+{
+public:
+	//===============================================
+	//	定数
+	//===============================================
+	enum class CLONE_TYPE
+	{
+		NORMAL,		// モデル
+		DIE,		// 破れるモデル
+		MAX
+	};
+
+private:
+
+	BasePlayer *players[PLAYER_MAX];						// プレイヤー実体
+	iex3DObj *clone_model[(int)CLONE_TYPE::MAX];			// clone元
+
+public:
+
+	//===============================================
+	//	初期化と解放
+	//===============================================
+	PlayerManager();
+	~PlayerManager();
+	void Initialize();
+	void Release();
+
+	//===============================================
+	//	更新と描画
+	//===============================================
+	void Update();
+	void Render();
+
+
+	//===============================================
+	//	Get&Set
+	//===============================================
+	BasePlayer *Get_player(int no)
+	{
+		assert(no >= 0 && no < PLAYER_MAX);
+		return players[no];
+	}
+
+};
+
+extern PlayerManager *player_mng;
