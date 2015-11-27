9061b403f35d98e141c4e666b894f24f3bbb2fa8
RikiyaHidaka <ridley.skirmisher@gmail.com>
2015-11-27 19:41:14 +0900

    消した

--- Paperona4ネット/paper1119/paper4/client/source/timer/Timer.cpp
+++
@@ -1,9 +0,0 @@
-#include "iextreme.h"
-#include "Timer.h"
-
-void Timer::Render()
-{
-	Text::Draw(600, 32, 0xff00ffff, "%d", limit_time);
-}
-
-Timer *timer;
--- Paperona4ネット/paper1119/paper4/server/source/fstream/fstream_paper.h
+++
@@ -1,9 +0,0 @@
-
-// fstream Šg’£
-
-#include <fstream>
-
-struct Vector3;
-
-std::fstream& operator >> (std::fstream&, Vector3&);
-std::ifstream& operator >> (std::ifstream&, Vector3&);
